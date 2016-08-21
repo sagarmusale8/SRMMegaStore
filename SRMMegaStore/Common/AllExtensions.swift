9dbb3815eb802f3e8a6118296f0ebf6fad18cad0
Sagar Musale <sagar1991.musale@gmail.com>
2016-08-22 00:32:06 +0530

    Added extension for View

---
+++ SRMMegaStore/Common/AllExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  AllExtensions.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 22/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+/*
+ *  Extensions for all elements will be here
+ */
+
+import UIKit
+
+// MARK: UIView
+extension UIView{
+    
+    // Getting view width
+    func viewWidth()->CGFloat{
+        return self.frame.size.width
+    }
+    
+    // Getting view width
+    func viewHeight()->CGFloat{
+        return self.frame.size.height
+    }
+}
+
