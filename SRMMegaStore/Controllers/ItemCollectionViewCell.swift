4afaf606914f7cf47e40b7e6683adc357531f93f
Sagar Musale <sagar1991.musale@gmail.com>
2016-08-21 19:38:26 +0530

    Changed the structure

---
+++ SRMMegaStore/Controllers/ItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCollectionViewCell.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 20/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgItem: UIImageView!
+    @IBOutlet weak var lblItem: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+}
---
+++ SRMMegaStore/Controllers/ItemListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ItemListViewController.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 20/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    @IBOutlet weak var collectionViewItems: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
