217af7ac4a486d72f02cca2a59d0e282ba53ae45
Sagar Musale <sagar1991.musale@gmail.com>
2016-08-21 23:30:52 +0530

    Showing all items in grid view

--- SRMMegaStore/Controllers/ItemListViewController.swift
+++ SRMMegaStore/Controllers/ItemListViewController.swift
@@ -8,20 +8,52 @@
 
 import UIKit
 
-class ItemListViewController: UIViewController {
+class ItemListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionViewItems: UICollectionView!
     
+    let reusableIdForItemCollectionViewCell = String(ItemCollectionViewCell)
+    
+    var allItems: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchAllItems()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    // MARK: Setting up UI Componenets
+    func setupUI(){
+        
     }
 
+    // MARK: Loading data from core data
+    func fetchAllItems(){
+        if let sortedItems = ItemsDataHandler.getAllItems(){
+            allItems = sortedItems
+        }
+        collectionViewItems.reloadData()
+    }
 
+    // MARK: UICollectionView DataSource Method
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return allItems.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        if let itemCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(reusableIdForItemCollectionViewCell, forIndexPath: indexPath) as? ItemCollectionViewCell{
+            let item = allItems[indexPath.row]
+            itemCollectionViewCell.lblItem.text = item.name
+            itemCollectionViewCell.lblPrice.text = String(item.price)
+            
+            return itemCollectionViewCell
+        }
+        return UICollectionViewCell()
+    }
 }
 
