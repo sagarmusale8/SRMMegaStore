aafc93a652aa820af959dc94f0a54ed4402aeaee
Sagar Musale <sagar1991.musale@gmail.com>
2016-08-22 13:32:29 +0530

    Added fonts and constants to have constants

---
+++ SRMMegaStore/Common/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 22/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+import UIKit
+
+class Colors: NSObject {
+    static let Green_609030 = UIColor.colorWithHex(0x609030)
+}
---
+++ SRMMegaStore/Common/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 22/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+import UIKit
+
+class Fonts: NSObject {
+    
+    static let Regular_18 = UIFont(name: "AvenirNext-Regular", size: 18.0)
+    
+    static let Medium_18 = UIFont(name: "AvenirNext-Medium", size: 18.0)
+}
