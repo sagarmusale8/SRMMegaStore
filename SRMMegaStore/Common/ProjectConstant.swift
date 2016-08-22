b21cd77ba77c91724607f454395b9972a8d4c7c0
Sagar Musale <sagar1991.musale@gmail.com>
2016-08-22 13:32:52 +0530

    Added project constants

---
+++ SRMMegaStore/Common/ProjectConstant.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectConstant.swift
+//  SRMMegaStore
+//
+//  Created by Sagar Musale on 22/08/16.
+//  Copyright © 2016 SRM. All rights reserved.
+//
+
+import UIKit
+
+class ProjectConstant: NSObject {
+
+    // ID of segue to push detail view 
+    static let SEGUE_DETIALS_ID = "show_details"
+}
